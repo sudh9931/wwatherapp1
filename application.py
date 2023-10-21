6557be2cf711091ab3f67ace4ad93a56ba57232d
sudh9931 <sskumar9876@gmail.com>
2023-10-21 20:06:12 +0530

    first commit

--- application.py
+++ application.py
@@ -1,89 +1,49 @@
 from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+#from flask_cors import CORS,cross_origin
 import requests
-from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
-import pymongo
 
 application = Flask(__name__) # initializing a flask app
 app=application
 
 @app.route('/',methods=['GET'])  # route to display the home page
-@cross_origin()
+#@cross_origin()
 def homePage():
     return render_template("index.html")
 
 @app.route('/review',methods=['POST','GET']) # route to show the review comments in a web UI
-@cross_origin()
-def index():
+#@cross_origin()
+def get_weather():
     if request.method == 'POST':
+        api_key = 'b2f7276693524dc1283929277faa0eae' 
+        base_url = 'https://api.openweathermap.org/data/2.5/weather'   
+        params = {
+            'q': request.form.get("content"),
+            'appid': api_key,
+            'units': 'metric'  # Use 'imperial' for Fahrenheit
+        }
+
         try:
-            searchString = request.form['content'].replace(" ","")
-            flipkart_url = "https://www.flipkart.com/search?q=" + searchString
-            uClient = uReq(flipkart_url)
-            flipkartPage = uClient.read()
-            uClient.close()
-            flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
-            del bigboxes[0:3]
-            box = bigboxes[0]
-            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
-            prodRes = requests.get(productLink)
-            prodRes.encoding='utf-8'
-            prod_html = bs(prodRes.text, "html.parser")
-            print(prod_html)
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
+            response = requests.get(base_url, params=params)
+            data = response.json()
 
-            filename = searchString + ".csv"
-            fw = open(filename, "w")
-            headers = "Product, Customer Name, Rating, Heading, Comment \n"
-            fw.write(headers)
-            reviews = []
-            for commentbox in commentboxes:
-                try:
-                    #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+            if response.status_code == 200:
+                city = data['name']
+                temperature = data['main']['temp']
+                description = data['weather'][0]['description']
 
-                except:
-                    name = 'No Name'
+                print(f'City: {city}')
+                print(f'Temperature: {temperature}°C')
+                print(f'Weather: {description}')
+            else:
+                print(f'Error: {data["message"]}')
 
-                try:
-                    #rating.encode(encoding='utf-8')
-                    rating = commentbox.div.div.div.div.text
-
-
-                except:
-                    rating = 'No Rating'
-
-                try:
-                    #commentHead.encode(encoding='utf-8')
-                    commentHead = commentbox.div.div.div.p.text
-
-                except:
-                    commentHead = 'No Comment Heading'
-                try:
-                    comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
-                    custComment = comtag[0].div.text
-                except Exception as e:
-                    print("Exception while creating dictionary: ",e)
-
-                mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
-                          "Comment": custComment}
-                reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
-            db = client['review_scrap']
-            review_col = db['review_scrap_data']
-            review_col.insert_many(reviews)
-            return render_template('results.html', reviews=reviews[0:(len(reviews)-1)])
-        except Exception as e:
-            print('The Exception message is: ',e)
-            return 'something is wrong'
-    # return render_template('results.html')
-
+        except requests.exceptions.RequestException as e:
+            print(f'Error: {e}')
+        return f'City: {city} ,Temperature: {temperature}°C ,Weather: {description}'
     else:
         return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000, debug=True)
-	#app.run(debug=True)
+    #app.run(host='0.0.0.0', port=8000)
+	app.run(debug=True)
